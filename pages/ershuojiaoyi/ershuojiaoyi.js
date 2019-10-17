e2c04ba5599d5a39508ee754925e77d5f5af3375
unknown <unknown@example.com>
2019-10-17 15:15:14 +0800

    Initial Commit

---
+++ pages/ershuojiaoyi/ershuojiaoyi.js
@@ -0,0 +1,75 @@
+// pages/ershuojiaoyi/ershuojiaoyi.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   ers1: '../../pages/index/images/ers1.jpg',
+   ers2: '../../pages/index/images/ers2.jpg',
+    ers3: '../../pages/index/images/ers3.jpg',
+    ers4: '../../pages/index/images/ers4.jpg',
+    ers5: '../../pages/index/images/ers5.jpg',
+    ers6: '../../pages/index/images/ers6.jpg',
+    ers7: '../../pages/index/images/ers7.jpg',
+    ers8: '../../pages/index/images/ers8.jpg',
+    ers9: '../../pages/index/images/ers9.jpg',
+    ers10: '../../pages/index/images/ers10.jpg'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
